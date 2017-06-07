cff127598ee083eed2d98213b1efe331db1bcaa8
Max <max@motovilov.com>
2017-06-07 17:05:37 -0500

    Test that hooks still work in Mocha

--- tests/mocha.js
+++ tests/mocha.js
@@ -1,10 +1,22 @@
-var cort = require( "../mocha" ); 
+const cort = require( "../mocha" ),
+      assert = require( "assert" ); 
 
 it = cort( it );
 
+beforeEach( () => console.log( "Before test" ) );
+afterEach( () => console.log( "After test" ) );
+
 describe( "cort/mocha tests", () => {
+
+    var constant;
+
+    beforeEach( () => constant = 1 );
+    afterEach( () => console.log( "Constant =", constant ) );
+
     it( "Minimal", function( later, done ) {
         var total = 3;
+
+        assert.equal( constant++, 1 );
 
         later( () => action( "A" ) );
 
